cce8bded6861b4c29cef1ef497005790171fdac7
ae2f <mincraft20046@gmail.com>
2025-04-01 09:30:17 +0000

    port

--- src/ann/Conv.c
+++ src/ann/Conv.c
@@ -57,46 +57,60 @@ _return:
 	return err;
 }
 
-typedef struct {
-	size_t dimi;
-	size_t i, j, outlen, inglen;
+typedef struct ConvPtm {
+		size_t
+			dimi, // dim, current
+
+
+			i, j, k,	
+			outlen,
+			outstridestart,
+			gapneeded;	// you need to give this in handler.
+		const ae2f_float_t
+			* infv,
+			* ingv;
+		ae2f_float_t
+			* outv; // Zero-initiating guaranteed (Suggestion).
 } ConvPrm_t;
+
 
 struct {
 	size_t 
 		pad,
 		dim,
-		* opt_retoutszrequired;
+		* opt_retoutszrequired; // size: dim
 
 	const size_t
-		* stride,
+		* stride, // size: dim
 		* lpinfc,
 		* lpingc,
 		* lpoutc;
-
-	const ae2f_float_t
-		* infv,
-		* ingv;
-
-	ae2f_float_t
-		* outv;
+	
+	ConvPrm_t* prmorig;
 	
 	// Down here is no parameters
 	ae2f_err_t err;
-
 }* ConvRet;
 
 /**
+ * @todo
+ * Indexing
+ *
  * @brief 
  * `dim` must be the dimension of mmaps, lengths of lists.
- * This function is meant to be recursive. For minimize the stack, 
+ * This function is meant to be recursive. For minimize the stack,
+ *
+ * @param prm
+ * Meant to be a vector.
+ * Size... unknown.
  * */
 ae2f_SHAREDEXPORT
 void
-__ae2f_AnnConv(ConvPrm_t* prm)
+__ae2f_AnnConv(size_t prmi)
+#define prm (ConvRet->prmorig + prmi)
 {
 	if(!ConvRet) return;
-	if(!prm) {
+	if(!ConvRet->prmorig) {
 		// no good, prm is null
 		ConvRet->err |= ae2f_errGlob_PTR_IS_NULL;
 		ConvRet = 0;
@@ -109,42 +123,58 @@ __ae2f_AnnConv(ConvPrm_t* prm)
 #define return(e) { ConvRet->err |= e; return; }
 
 	if(!ConvRet->stride)		return(ae2f_errGlob_PTR_IS_NULL);
-
 	if(!ConvRet->lpinfc)		return(ae2f_errGlob_PTR_IS_NULL);
 	if(!ConvRet->lpingc)		return(ae2f_errGlob_PTR_IS_NULL);
 	if(!ConvRet->lpoutc)		return(ae2f_errGlob_PTR_IS_NULL);
+	if(!prm->infv)			return(ae2f_errGlob_PTR_IS_NULL);
+	if(!prm->ingv)			return(ae2f_errGlob_PTR_IS_NULL);
+	if(!prm->outv)			return(ae2f_errGlob_PTR_IS_NULL);
+	if(ConvRet->dim <= prm->dimi)	return(ae2f_errGlob_WRONG_OPERATION);
 
-	if(!ConvRet->infv)		return(ae2f_errGlob_PTR_IS_NULL);
-	if(!ConvRet->ingv)		return(ae2f_errGlob_PTR_IS_NULL);
-	if(!ConvRet->outv)		return(ae2f_errGlob_PTR_IS_NULL);
-
-	if(ConvRet->dim - prm->dimi == 1)
+	if(prm->dimi == 1)
 	{
 		// It is just single layer.
 		// Ending by calling 1d.
-		ConvRet->err = ae2f_AnnConv1d(
-				ConvRet->infv,		*ConvRet->lpinfc
-				, ConvRet->ingv,	*ConvRet->lpingc
-				, ConvRet->outv,	ConvRet->opt_retoutszrequired
+		ConvRet->err |= 
+			ae2f_AnnConv1d(
+				prm->infv,		*ConvRet->lpinfc
+				, prm->ingv,		*ConvRet->lpingc
+				, prm->outv,		ConvRet->opt_retoutszrequired
 				, *ConvRet->stride,	ConvRet->pad
 				);
 
 		// operation is over
-		ConvRet->prm = 0;
+		ConvRet = 0;
 		return;
 	}
 
-	if(prm->out.dim < prm->dim) return (ae2f_errGlob_WRONG_OPERATION);
+	prm->outlen = (
+		 ConvRet->lpinfc[prm->dimi]
+		 + (ConvRet->pad << 1) 
+		 - ConvRet->lpingc[prm->dimi]
+		 )
+		/ (ConvRet->stride[prm->dimi]) 
+		+ 1
+		;
 
-	prm->outlen = ConvRet->pad - prm->out.c[prm->dim - 1];
-	prm->inglen = ConvRet->pad - prm->ing.c[prm->dim - 1];
+	if(ConvRet->opt_retoutszrequired)
+		*ConvRet->opt_retoutszrequired = prm->outlen;
 
 	for(prm->i = 0; prm->i < prm->outlen; prm->i++)
 	{
+		prm->outstridestart = prm->i * ConvRet->stride[prm->dimi];
 
-		for(prm->j = 0; prm->j < prm->inglen; prm->j++)
+		// This is zero-padded layer. Skip it thinking 
+		if(prm->outstridestart < ConvRet->pad || ConvRet->lpoutc[prm->dimi] - prm->outstridestart)
+			continue;
+
+		for(prm->j = 0; prm->j < ConvRet->lpingc[prm->dimi]; prm->j++)
+#define prmnxt ConvRet[prmi + (prm->j * prm->outlen + prm->i) * prm->gapneeded]
 		{
 
+			__ae2f_AnnConv(prm + 1);
+#undef prmnxt
 		}
 	}
+#undef return
 }
